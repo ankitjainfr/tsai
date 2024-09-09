46996b825ad0c24ba9d0044d939145348b8f288d
ankitjainfr <109954181+ankitjainfr@users.noreply.github.com>
2024-09-09 18:21:55 +0530

    nw

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,62 +1,61 @@
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const app = express();
-const http = require("http").createServer(app);
-const bodyParser = require("body-parser");
-
-app.use(cors());
-app.use(express.json());
-const API_PORT = process.env.PORT || 3003;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// app.use(logger("dev"));
-
-http.listen(API_PORT, () => {
-  console.log(`LISTENING ON PORT ${API_PORT}`);
-});
-
 const { Bot, InlineKeyboard } = require("grammy");
-const botToken = process.env.BOT_TOKEN;
-const bot = new Bot(botToken);
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const TOKEN = '7435621483:AAGRUIjDzAJdKTwAThDbgwQyNJ96WSTm3KI';
+const URL = 'https://turboswap2.vercel.app/';
 
+const token = process.env.BOT_TOKEN;
+
+console.log(token);
+if (!token) throw new Error("BOT_TOKEN is unset");
+
+const bot = new Bot(token);
+
+const app = express();
+app.use(bodyParser.json());
+
+// Handle the /start command
 bot.command("start", async (ctx) => {
-  const userid = ctx.from.username; // Get the Telegram user ID
-  console.log("userid:", userid);
-  const receiveid = ctx.match;
-  console.log("receiveid:", receiveid);
+  console.log("/start command received");
 
-  const menus = new InlineKeyboard().webApp(
-    "Click to get started!",
-    "https://turboswap2.vercel.app/"
-  );
 
+  // Reply with personalized message and horizontal inline keyboard
   await ctx.replyWithPhoto(
-    "https://ipfs.io/ipfs/QmTvM9LXzgCuFTrWzJCHVFiMwUpu3rctYB6pt8nMadZoos?format=jpg&name=large",
+    "Welcome to TurboSwap AI!",
     {
-      reply_markup: menus,
-      parse_mode: "HTML",
-      caption: `Hello, @${userid}! Welcome to TurboWap AI.`,
+      caption: ``,
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Play",
+              url: "https://turboswap2.vercel.app", // Link to your Next.js app
+            },
+            {
+              text: "X",
+              url: "https://x.com/TurboSwapAI", // Link to x
+            },
+            {
+              text: "TG",
+              url: "https://t.me/TurboSwapAI_ton", // Replace with your community link
+            },
+          ],
+        ],
+      },
     }
   );
 });
 
-bot.on("callback_query:data", async (ctx) => {
-  const data = ctx.callbackQuery.data;
-  switch (data) {
-    case "howToEarn":
-      const menus = new InlineKeyboard().webApp(
-        "Click to get started!",
-        "https://turboswap2.vercel.app/"
-      );
-      await ctx.reply("Hello, this is just example.", {
-        reply_markup: menus,
-        parse_mode: "HTML",
-      });
-    default:
-      break;
-  }
+// Start the bot.
+bot.start();
+
+app.post(`/bot${token}`, (req, res) => {
+  bot.handleUpdate(req.body, res);
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the bot server!");
 });
 
 app.listen(5000, async () => {
